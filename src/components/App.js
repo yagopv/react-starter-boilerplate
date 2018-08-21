58f6da0b78f31d4cbe5c3fa1d432fe5e4f5967f5
Yago Pérez Vázquez <yagoperez@outlook.com>
2018-08-21 23:42:03 +0200

    [FIX] Error

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import { square } from '../math';
 
 class App extends Component {
   
   render() {
-    const x = square(2);
     return (
       <div>
         <h1>Starter template</h1>
