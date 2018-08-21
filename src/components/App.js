960d4ea37b96dda0b5f3910b17b6fac7defb54fa
Yago Pérez Vázquez <yagoperez@outlook.com>
2018-08-21 23:39:25 +0200

    [ADD] Finishing project boilerplate base config

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { square } from '../math';
 
 class App extends Component {
+  
   render() {
+    const x = square(2);
     return (
       <div>
         <h1>Starter template</h1>
